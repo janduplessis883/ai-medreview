317e291000eb54c5cbb541ffa550e9a59391dac4
Jan du Plessis <drjanduplessis@icloud.com>
2024-04-06 21:11:15 +0100

    Automated commit via Python script - 2024-04-06 21:11

--- ai_medreview/scheduler.py
+++ ai_medreview/scheduler.py
@@ -8,13 +8,13 @@ init()  # Initializes Colorama
 wait_time = 7200  # 2 hours
 
 while True:
-    subprocess.run(["python", "friendsfamilytest/data.py"])
+    subprocess.run(["python", "ai_medreview/data.py"])
 
     # Countdown for wait_time
     for remaining in range(wait_time, 0, -1):
         sys.stdout.write("\r")
         # Setting the color to bold red for the countdown
-        countdown_message = "{0}{1}{2} seconds remaining. Friends & Family Test".format(
+        countdown_message = "{0}{1}{2} seconds remaining. AI MedReview".format(
             Fore.RED + Style.BRIGHT, remaining, Style.RESET_ALL
         )
         sys.stdout.write(countdown_message)
