21ff14d2c6bcfc2fd5a589c5fee07094b6b94168
Jan du Plessis <drjanduplessis@icloud.com>
2024-04-30 12:35:32 +0100

    Automated commit via Python script - 2024-04-30 12:35

--- ai_medreview/data.py
+++ ai_medreview/data.py
@@ -374,7 +374,7 @@ def concat_save_final_df(processed_df, new_df):
 
 @time_it
 def load_local_data():
-    df = pd.read_csv(f"{DATA_PATH}/data.csv")
+    df = pd.read_csv(f"{DATA_PATH}/data.csv", encoding='latin1')
     df["time"] = pd.to_datetime(df["time"], dayfirst=True)
     return df
 
