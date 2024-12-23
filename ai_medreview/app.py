99e0a2ae25e420ae73b38edad845773c773d603e
Jan du Plessis <drjanduplessis@icloud.com>
2024-12-23 22:36:28 +0000

    Automated commit via Python script - 2024-12-23 22:36

--- ai_medreview/app.py
+++ ai_medreview/app.py
@@ -29,7 +29,7 @@ client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 # Simple function to get a response from Groq
 @st.cache_resource
-def ask_groq(prompt: str, model: str = "llama-3.1-8b-instant"):
+def ask_groq(prompt: str, model: str = "llama-3.3-70b-versatile"):
     chat_completion = client.chat.completions.create(
         messages=[
             {
